9261a4db0f4a37eae8f462c9af06a1a06cece67b
Isaiah Praise Ogooluwa Bakare <92177420+Praiseogooluwa@users.noreply.github.com>
2025-07-23 17:35:46 -0400

    Update SpamDetector.tsx

--- frontend/spam-ai/src/components/SpamDetector.tsx
+++ frontend/spam-ai/src/components/SpamDetector.tsx
@@ -29,7 +29,7 @@ const SpamDetector = () => {
     setMounted(true);
   }, []);
 
-  const API_BASE_URL = 'http://127.0.0.1:8000';
+  const API_BASE_URL = 'https://spam-detector-pzin.onrender.com';
 
   const analyzeEmail = async () => {
     if (!emailText.trim() || !senderEmail.trim()) {
